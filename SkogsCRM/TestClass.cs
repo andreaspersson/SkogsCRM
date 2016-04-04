7e4ef01193c9dccdce515f0c0ccabe3c83181acf
Hittebarnet <rasmustarland@hotmail.com>
2016-04-04 19:21:23 +0200

    Fixad testmetod

--- SkogsCRM/TestClass.cs
+++ SkogsCRM/TestClass.cs
@@ -4,14 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SkogsCRM
-{
-    class TestClass
-    {
-        SalesAgent sa = new SalesAgent();
-        Customer c = new Customer();
-        ForestEstate forestEstate = new ForestEstate();
-        SkogsDBEntities se = new SkogsDBEntities();
-   
+namespace SkogsCRM {
+    class TestClass {
+        public static void Main()
+        {
+            SalesAgent sa = new SalesAgent
+            {
+                firstName = "Rolf",
+                surname = "Sturesson",
+                employeeId = 2,
+                telephoneNbr = "0736348934"
+            };
+
+            SkogsDBEntities lol = new SkogsDBEntities();
+            SalesAgent s1 = lol.SalesAgent.Find(2);
+            Console.WriteLine(s1.firstName);
+            Console.WriteLine(s1.surname);
+            Console.ReadLine();
+        }
     }
 }
