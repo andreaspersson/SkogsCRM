827c99d81cf03f65bc7263cd196a9a825220c358
Hittebarnet <rasmustarland@hotmail.com>
2016-04-04 19:51:04 +0200

    Fler testmetoder

--- SkogsCRM/TestClass.cs
+++ SkogsCRM/TestClass.cs
@@ -6,13 +6,15 @@ using System.Threading.Tasks;
 
 namespace SkogsCRM {
     class TestClass {
+        //Solution Explorer -> Högerklick på SkogsCRM -> Properties -> Ändra Startup Object till testClass
+        //Ändra också Output type till Console Application om ni pallar.
         public static void Main()
         {
             SalesAgent sa = new SalesAgent
             {
                 firstName = "Rolf",
                 surname = "Sturesson",
-                employeeId = 2,
+                employeeId = 3,
                 telephoneNbr = "0736348934"
             };
 
@@ -20,7 +22,18 @@ namespace SkogsCRM {
             SalesAgent s1 = lol.SalesAgent.Find(2);
             Console.WriteLine(s1.firstName);
             Console.WriteLine(s1.surname);
+            //
+            //lol.SalesAgent.Add(sa);
+            //lol.SaveChanges();
+            //Ovan fungerar för att lägga till skit i DBn
+
+            ICollection<Customer> cCollection = s1.Customer;
+            foreach (Customer c in cCollection)
+            {
+                Console.WriteLine(c.firstName);
+            }
             Console.ReadLine();
+
         }
     }
 }
