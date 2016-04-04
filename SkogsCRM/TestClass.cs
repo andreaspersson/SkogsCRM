7c0ef4571b38cb43961a3f9016b2c7019f4839eb
Hittebarnet <rasmustarland@hotmail.com>
2016-04-04 18:13:48 +0200

    Testklass

---
+++ SkogsCRM/TestClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkogsCRM
+{
+    class TestClass
+    {
+        SalesAgent sa = new SalesAgent();
+        Customer c = new Customer();
+        ForestEstate forestEstate = new ForestEstate();
+        SkogsDBEntities se = new SkogsDBEntities();
+   
+    }
+}
