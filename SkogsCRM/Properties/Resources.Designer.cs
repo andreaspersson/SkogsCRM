dee11d803f5742058a0767a9cd7c0eb926c49898
Hittebarnet <rasmustarland@hotmail.com>
2016-04-06 16:20:07 +0200

    ui

--- SkogsCRM/Properties/Resources.Designer.cs
+++ SkogsCRM/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace SkogsCRM.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap logomedium {
+            get {
+                object obj = ResourceManager.GetObject("logomedium", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
